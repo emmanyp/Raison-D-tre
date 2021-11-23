4064285845b7077d3dbe300e9632f732b1327416
emma <emma.ronaldo7@gmail.com>
2021-11-22 22:30:33 -0500

    render  index html successfully

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,21 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+def places_index(request):
+  return render(request, 'places/index.html', {'places': places})
+
+class Place:
+  def __init__(self, name, location, description, start_date, end_date):
+    self.name = name
+    self.location = location
+    self.description = description
+    self.start_date = start_date
+    self.end_date = end_date
+
+places = [
+  Place('dear Han', 'NYC NY', 'cafe', 'april 2020', 'Aug 2021' ),
+  Place('dear Han', 'NYC NY', 'cafe', 'april 2020', 'Aug 2021' ),
+  Place('dear Han', 'NYC NY', 'cafe', 'april 2020', 'Aug 2021' ),
+]
