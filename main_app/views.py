380b96c2841058e3e572c30c93038dd221ae7fef
emma <emma.ronaldo7@gmail.com>
2021-11-23 10:11:14 -0500

    render place detail

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,7 @@ from .models import Place
 def home(request):
   return render(request, 'home.html')
 
+
 def about(request):
   return render(request, 'about.html')
 
@@ -15,3 +16,7 @@ def places_index(request):
   places = Place.objects.all()
   return render(request, 'places/index.html', {'places': places})
 
+
+def places_detail(request, place_id):
+  place = Place.objects.get(id=place_id)
+  return render(request, 'places/detail.html', { 'place': place })
