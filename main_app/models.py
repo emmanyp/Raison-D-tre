d5f6d4c15ca48bd725f702e0418511559dc6d32e
emma <emma.ronaldo7@gmail.com>
2021-11-23 12:16:00 -0500

    successfully added date picker

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 # Create your models here.
 
 class Place(models.Model):
   name = models.CharField(max_length=100)
   location = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
-  start_date = models.DateField()
-  end_date = models.DateField()
+  date = models.DateField('Start Date')
+  end_date =  models.DateField('End Date')
 
   def __str__(self):
     return self.name
 
   def get_absolute_url(self):
     return reverse('places_detail', kwargs={'place_id': self.id})
-
-    
