c3a3fb10e75fe0bdda11980d982a7daa5254c157
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-05-25 15:34:09 -0500

    added test driver

---
+++ Test/test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+#Grabbing the Browser
+driver = webdriver.Chrome()
+#Opening the Browser for the app
+driver.get("http://127.0.0.1:8000/")
+
+assert driver.title == "Raison D'être"
+
+
+
+
+
+
+#Close the Browser
+driver.quit()
