5da623841e7624fd4f088b0bdb78b1d4c652cda4
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-05-25 17:30:26 -0500

    testing github actions 4

--- Test/test.py
+++ Test/test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_title():
-    driver = webdriver.Chrome('./chromedriver.exe')
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://raison-detre.herokuapp.com")
     assert driver.title == "Raison D'être", "should be 'Raison D'être'"
     driver.quit()
