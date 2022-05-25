262685862f16e8cfde0be4f4a2224db96c30ab54
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-05-25 18:10:38 -0500

    help

--- Test/test.py
+++ Test/test.py
@@ -1,18 +1,7 @@
 from selenium import webdriver
-import sys
-import os
-
-
-
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.dirname(__file__)
-    return os.path.join(base_path, relative_path)
 
 def test_title():
-    driver = webdriver.Chrome(resource_path('./chromedriver.exe'))
+    driver = webdriver.Chrome()
     driver.get("https://raison-detre.herokuapp.com")
     assert driver.title == "Raison D'être", "should be 'Raison D'être'"
     driver.quit()
